632d2089702dc98ef93b1e194d91cf1b184421ab
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-10-18 21:09:43 +0300

    Ifai

---
+++ _20191018_IfelseIFDalis/_20191018_IfelseIFDalis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191018_IfelseIFDalis
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sk1 = 50;
+            int sk2 = 20;
+            Console.WriteLine("Iveskite studento amziu");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            if (amzius > sk1)
+            {
+                Console.WriteLine("Amzius yra didesnis nei pirmas skaicius");
+            }
+            else if (amzius > sk2)
+            {
+                Console.WriteLine("Amzius yra didesnis nei antras skaicius");
+            }
+            else
+            {
+                Console.WriteLine("Amzius nera didesnis nei uz viena skaiciu");
+            }
+        }
+    }
+}
---
+++ _20191018_skaiciavimoMasinele/_20191018_skaiciavimoMasinele/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191018_skaiciavimoMasinele
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite zenkla");
+            char zenklas = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine("\nIveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+
+            if (zenklas == '+')
+            {
+                Console.WriteLine("sudetis yra " + (sk + sk1));
+            }
+            else if (zenklas == '-')
+            {
+                Console.WriteLine("atimtis yra " + (sk - sk1));
+            }
+            else if (zenklas == '*')
+            {
+                Console.WriteLine("sandauga yra " + (sk * sk1));
+            }
+            else if (zenklas == '/')
+            {
+                if (sk1 != 0)
+                {
+                    Console.WriteLine("dalyba yra " + ((double)sk / sk1));
+                }
+                else
+                {
+                    Console.WriteLine("Dalyba is nulio negalima");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Neatpazintas zenklas");
+            }
+        }
+    }
+}
