82668ed19664858fe47b2fd851c9969f8f6be4e7
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-10-18 19:06:57 +0300

    Pirmas uzdavinys

---
+++ _20191018_pirmasPasikartojimoUzdavinys/_20191018_pirmasPasikartojimoUzdavinys/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191018_pirmasPasikartojimoUzdavinys
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite zodi");
+            string zodis = Console.ReadLine();
+            Console.WriteLine("#" + zodis + "#");
+        }
+    }
+}
