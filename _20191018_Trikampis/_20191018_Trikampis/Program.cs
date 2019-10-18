94ef0416e869047e22353400e81a94b5eb49df88
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-10-18 21:22:08 +0300

    Trikampiai

---
+++ _20191018_Trikampis/_20191018_Trikampis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191018_Trikampis
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int c = Convert.ToInt32(Console.ReadLine());
+            // && => reiskia ir
+            // || => reiskia arba
+            if (a + b > c && b + c > a && a + c > b)
+            {
+                Console.WriteLine("Sudaryti trikampi galima");
+            }
+            else
+            {
+                Console.WriteLine("Sudaryti trikampio negalima");
+            }
+        }
+    }
+}
